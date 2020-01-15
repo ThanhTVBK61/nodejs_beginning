bbf780b244ea12b90512f120617de987a553c34f
ThanhTVBK61 <thanh.tvbk61@gmail.com>
2020-01-16 04:32:00 +0700

    Delete folder web

--- Web/router.js
+++
@@ -1,9 +0,0 @@
-function route(handle, pathName) {
-    if (typeof handle[pathName] === 'function') {
-        handle[pathName]();
-    } else {
-        console.log("Error 404: Not Found!");
-    }
-}
-
-exports.route = route;
